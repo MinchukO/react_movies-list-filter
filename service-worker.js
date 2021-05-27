080f12b53b132e807a8ea2de177034651e460fd8
Minchuk Olga <olga@MacBook-Pro-Olga.local>
2021-05-27 04:18:38 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.712f862ba1e18ab5cc48590a2b7ed47e.js"
 );
 
 self.addEventListener('message', (event) => {
